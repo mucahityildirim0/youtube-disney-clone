220b9c79d2d64a384221704fb43002202aeadb41
Mücahit Yıldırım <mucahityildirim@Mucahit-MacBook-Air.local>
2022-05-29 13:05:32 +0300

    dev eklemesi yaptım

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Youtube disney+ klon projesi yapıyorum</div>;
+  return (
+    <div className="App">
+      Youtube disney+ klon projesi yapıyorum
+      <h1>DEV BRANCH</h1>
+    </div>
+  );
 }
 
 export default App;
